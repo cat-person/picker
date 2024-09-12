924414683ca42e823cce4dc83cb737ba8d39c962
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-09-12 19:56:28 +0400

    Contacts should be printed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
 var App = {
-    propertiesBtn: null,
-    selectBtn: null,
-    msg: "output",
+    getContactBtn: null,
     init() {
         console.log("App started");
         // this.initEvents();
         
+        msg = "output";
+
         if (this.isContactSelectApiAvailable()) {
             msg = "Contact select api is available";
         } else {
             msg = "!!! Contact select api is not available";
         }
+        console.log(msg)
+        
 
         document.getElementById("output").textContent = msg;
-        console.log(msg)
+        document.getElementById("btn_get_contact").onclick = function() {
+            "get contacts clicked";
+            // Check if the device supports the contacts API
+            if (navigator.contacts) {
+                // Create a new contact picker instance
+                var contactPicker = new ContactPicker();
+            
+                // Set the callback function to handle the selected contacts
+                contactPicker.show(function(contacts) {
+                    // Do something with the selected contacts
+                    document.getElementById("output").textContent = contacts;
+                });
+            } else {
+                // Handle the case where the device does not support the contacts API
+                console.log("Device does not support contacts API");
+            }
+
+        };
     },
     isContactSelectApiAvailable() {
         return ('contacts' in navigator && 'ContactsManager' in window);
