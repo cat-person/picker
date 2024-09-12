112cdd347bc301dd03a9432782c45eecf31fe37f
Aleksei Klyshnniov <alexey.klyshnikov@gmail.com>
2024-09-12 19:40:49 +0400

    Output

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 var App = {
-propertiesBtn: null,
-selectBtn: null,
+    propertiesBtn: null,
+    selectBtn: null,
+    msg: "output",
     init() {
         console.log("App started");
         // this.initEvents();
+        
         if (this.isContactSelectApiAvailable()) {
-            console.log("Contact select api is available");
+            msg = "Contact select api is available";
         } else {
-            console.log("!!! Contact select api is not available");
+            msg = "!!! Contact select api is not available";
         }
+
+        document.getElementById("output").textContent = msg;
+        console.log(msg)
     },
     isContactSelectApiAvailable() {
         return ('contacts' in navigator && 'ContactsManager' in window);
     },
 };
-  
-  window.onload = App.init.bind(App);
+
+window.onload = App.init.bind(App);
